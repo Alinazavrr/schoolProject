e16dc146949a08d98e12442a05811e49945ef6ea
Alinazavrr <gorsun98@gmail.com>
2023-10-12 09:22:23 +0300

    12.10

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Type:
+    name = models.CharField(max_length=200)
+
+class Diets:
+    name = models.CharField(max_length=200)
+    overview = models.TextField()
+    colories = models.IntegerField()
+    type = models.ManyToManyField(Type, on_delete=models.CASCADE)
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='main_page'),
-
+    path('diets/', views.HomePageView.as_view(), name='diets_page'),
+    path('favorites/', views.HomePageView.as_view(), name='favorites_page'),
+    path('account/', views.HomePageView.as_view(), name='account_page'),
 ]
