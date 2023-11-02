23dfd4beef4d0aa429c5dad409ca33484392faf8
Жаркын Алиназар Омиржанулы <ZharkynA@lcoast.itstep>
2023-11-02 20:20:34 +0600

    commit

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='main_page'),
-    path('diets/', views.DietsView.as_view(), name='diets_page'),
-    path('favorites/', views.HomePageView.as_view(), name='favorites_page'),
-    path('account/', views.HomePageView.as_view(), name='account_page'),
+    path('diets/', views.DietsListView.as_view(), name='diets_page'),
+    path('favorites/', views.FavoritesDietsListView.as_view(), name='favorites_page'),
+    path('account/<int:pk>', views.AccountDetailView.as_view(), name='account_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 
 from .models import Diets, FavoritesDiets
 
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
+from django.contrib.auth.models import User
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -26,14 +28,22 @@ class HomePageView(TemplateView):
         return context
 
 
-class DietsView(LoginRequiredMixin, ListView):
+class DietsListView(LoginRequiredMixin, ListView):
     model = Diets
     context_object_name = 'diets'
-    template_name = 'main/homePage.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+    template_name = 'main/dietsPage.html'
 
 
-class FavoriteView(LoginRequiredMixin, ListView):
+class FavoritesDietsListView(LoginRequiredMixin, ListView):
+    model = FavoritesDiets
+    context_object_name = 'favorite'
+    template_name = 'main/favoritePage.html'
+
+
+class AccountDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'userDetailPage.html'
+    context_object_name = 'user'
+
+    def get_object(self, queryset=None):
+        return self.request.user
